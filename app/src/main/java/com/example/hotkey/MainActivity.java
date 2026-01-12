60d1859bcd3dade1eca2ac187ea5db1d7f878767
hhy <13612276628@163.com>
2026-01-12 15:28:48 +0800

    1.0

--- app/src/main/java/com/example/hotkey/MainActivity.java
+++ app/src/main/java/com/example/hotkey/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.hotkey;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class MainActivity extends AppCompatActivity {
+    EditText I;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.main);
+        I=findViewById(R.id.i);
+    }
+
+    public void input(View v) {
+        if(I.getText().toString().equals(""))return;
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("10.21.148.185", 1234);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out.println(I.getText().toString());
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
